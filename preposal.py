a5699c1595e7e38023cd711aa4e12305ca437142
ShahirKhan0314 <132020979+ShahirKhan0314@users.noreply.github.com>
2023-04-27 19:06:24 -0400

    The preposal

---
+++ preposal.py
@@ -0,0 +1,20 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+#TODO
+
+#tic tac toe and try to make it look nice
+#Mario with a ending victory screen
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+#TODO
+
+#no classmates involved
+
+## Do you have any questions of your own?
+
+#TODO
+
+#Not as of right now
